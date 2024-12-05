a98cdc6671e7b5a0ab90aa48ca4918b773116c48
sueyoshi ayaka <a.sueyoshi.eng@gmail.com>
2024-12-05 14:35:54 +0900

    web.php訂正

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,19 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategorieController
+use App\Http\Controllers\CategorieController;
 
 
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/', [ContactController::class, 'index']);
-Route::get('/contacts/confirm', [ContactController::class, 'confirm']);
-Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::get('/confirm', [ContactController::class, 'confirm']);
+Route::post('/confirm', [ContactController::class, 'confirm']);
 Route::get('/contacts', [ContactController::class, 'store']);
 Route::post('/contacts', [ContactController::class, 'store']);
-Route::get('/top', [AuthController::class, 'top']);
-Route::post('/top', [AuthController::class, 'top']);
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'register']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/', [CategorieController::class, 'index']);
