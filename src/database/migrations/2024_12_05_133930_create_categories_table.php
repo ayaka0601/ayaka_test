43980b9f480e121d6c0629af7653af94afa17c33
sueyoshi ayaka <a.sueyoshi.eng@gmail.com>
2024-12-05 13:51:39 +0900

    categoriestable変更

--- src/database/migrations/2024_12_05_133930_create_categories_table.php
+++ src/database/migrations/2024_12_05_133930_create_categories_table.php
@@ -17,7 +17,6 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->string('content');
             $table->timestamps();
-            $table->timestamps(updated_at);
         });
     }
 
